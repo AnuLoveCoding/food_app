779e0d46152c5b13489f34feead4f3b58c7050b4
Anurag <anusrivas07@gmail.com>
2023-05-01 21:55:17 +0530

    'food_app project'

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -10,26 +10,24 @@ class SignIn extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         // backgroundColor: Colors.blue,
-        title: Text('Online food Store',style: TextStyle(color: Colors.white),),
-        leading: ElevatedButton(onPressed: (){
-
-          }, child: Icon(Icons.home)),
+        title: const Text('Online Vegetables',style: TextStyle(color: Colors.white),),
+        leading:  Icon(Icons.home),
         elevation: 2,
-        actions: [
-          ElevatedButton(onPressed: (){
-
-            }, child: Icon(Icons.search,color: Colors.white,)),
-          SizedBox(height: 10.0,),
-          ElevatedButton(onPressed: (){
-
-            }, child: Icon(Icons.more_vert)),
-        ],
+        // actions: [
+        //   ElevatedButton(onPressed: (){
+        //
+        //     }, child: Icon(Icons.search,color: Colors.white,)),
+        //   SizedBox(height: 10.0,),
+        //   ElevatedButton(onPressed: (){
+        //
+        //     }, child: Icon(Icons.more_vert)),
+        // ],
       ),
       body: Container(
         height: double.infinity,
         width: double.infinity,
         decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage('../images/cald.jpg'),fit: BoxFit.cover)
+          image: DecorationImage(image: AssetImage('images/cald.jpg'),fit: BoxFit.cover)
         ),
 
         child: Column(
@@ -40,7 +38,7 @@ class SignIn extends StatelessWidget {
                     constraints: BoxConstraints.expand(width: double.infinity,height: 100.0),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                      image: DecorationImage(image: AssetImage('../images/eat.jpg'),fit: BoxFit.cover),
+                      image: DecorationImage(image: AssetImage('images/eat.jpg'),fit: BoxFit.cover),
                     ),
                   ),
                 SizedBox(height: 20.0,),
@@ -53,7 +51,7 @@ class SignIn extends StatelessWidget {
                       Text('Welcom to Vegg Online Store'),
                       SizedBox(height: 10.0,),
                       Text('Vegi', style: TextStyle(fontSize: 50.0,color: Colors.white,shadows: [BoxShadow(blurRadius: 5,color: Colors.green.shade900,offset: Offset(3, 5))]),),
-                      SizedBox(height: 150.0,),
+                      SizedBox(height: 100.0,),
                       Column(
                         children: [
                           SignInButton(Buttons.Apple, text: "Sign up with Apple" ,
--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,10 +1,102 @@
 import 'package:flutter/material.dart';
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
 
+
+class HomeScreen extends StatelessWidget {
+
+  Widget singalProducts(){
+    return Container(
+      height: 260,
+      width: 160,
+      decoration: BoxDecoration(
+          color: Color(0xffd9dad9) ,
+          borderRadius: BorderRadius.circular(10.0)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Image(
+              image: AssetImage('images/basil.jpg'),fit: BoxFit.cover,),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Fresh Basil',style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+                  Text('50\$/50 Gram',style: TextStyle(color: Colors.grey),),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.only(left: 10),
+                          height: 30.0,
+                          width: 50.0,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.grey,
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(6),),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  '50 Gram',
+                                  style: TextStyle(fontSize: 10),
+                                ),
+                              ),
+                              SizedBox(width: 3.0,),
+                              Center(
+                                child: Icon(
+                                  Icons.arrow_drop_down,
+                                  size: 15,
+                                  color: Colors.black,),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 5,),
+                      Expanded(
+                        child: Container(
+                          height: 30.0,
+                          width: 50.0,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.grey
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(8),),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.remove,size: 15.0,color: Color(0xffd0b84c),),
+                              SizedBox(width: 3.0,),
+                              Text('1',style:TextStyle(fontWeight: FontWeight.bold,color: Color(0xffd0b84c)),),
+                              SizedBox(width: 3.0,),
+                              Icon(Icons.add,size: 15.0,color: Color(0xffd0b84c)),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffcbcbcb),
       drawer: Drawer(),
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.black),
@@ -36,15 +128,15 @@ class HomeScreen extends StatelessWidget {
               height: 150.0,
               // constraints: BoxConstraints.expand(width: double.infinity,height: 100.0),
               decoration: BoxDecoration(
-                color: Colors.red,
                borderRadius: BorderRadius.all(Radius.circular(10.0),),
-                image: DecorationImage(image: AssetImage('../images/vege01.jpg'),fit: BoxFit.cover,)
+                image: DecorationImage(image: AssetImage('images/vege01.jpg'),fit: BoxFit.cover,)
               ),
               child: Row(
                 children: [
                   Expanded(
                     child: Container(
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                        Padding(
                          padding: const EdgeInsets.only(right: 190.0,bottom: 10.0),
@@ -57,13 +149,13 @@ class HomeScreen extends StatelessWidget {
                                bottomLeft: Radius.circular(50.0,),
                                bottomRight: Radius.circular(60.0,),)
                            ),
-                           child: Text('Vegi',style: TextStyle(fontSize: 20.0,color: Colors.white,shadows: [BoxShadow(color: Colors.green,blurRadius: 10,offset: Offset(3, 3)),]),),
+                           child: Text('Vegi',style: TextStyle(fontSize: 10.0,color: Colors.white,shadows: [BoxShadow(color: Colors.green,blurRadius: 10,offset: Offset(3, 3)),]),),
                          ),
                        ),
-                        Text('30% off', style: TextStyle(color: Colors.white,fontSize: 40.0,fontWeight: FontWeight.bold,shadows: [BoxShadow(offset: Offset(3,3),color: Colors.green)]),),
+                       Text('30% off', style: TextStyle(color: Colors.white,fontSize: 20.0,fontWeight: FontWeight.bold,shadows: [BoxShadow(offset: Offset(3,3),color: Colors.green)]),),
                         Padding(
                           padding: const EdgeInsets.only(left: 20.0),
-                          child: Text('on all vegetables products', style: TextStyle(color: Colors.white,fontSize: 15.0,fontWeight: FontWeight.bold),),
+                          child: Text('on all vegetables products', style: TextStyle(color: Colors.white,fontSize: 15.0,),),
                         ),
 
                       ],
@@ -83,6 +175,7 @@ class HomeScreen extends StatelessWidget {
                   ],
               ),
             ),
+            SizedBox(height: 5.0,),
             Row(
               children: [
 

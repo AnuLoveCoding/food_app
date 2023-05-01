d5a2ff29b16802a0ae21b740a8cec04a681e0d5a
Anurag <anusrivas07@gmail.com>
2023-05-01 17:31:16 +0530

    'food_app project'

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/screens/home_screen/home_screen.dart';
 
 import 'auth/sign_in.dart';
 
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: SignIn(),
+      home: HomeScreen(),
     );
   }
 }
